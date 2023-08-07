21c5be35500212b0998e5bfec50cee6892bb52c1
Seb Norris <sebastian@automationlogic.com>
2023-08-07 11:46:22 +0100

    add explicit dependson for ad log sub

--- terraform/environments/delius-iaps/ad.tf
+++ terraform/environments/delius-iaps/ad.tf
@@ -76,7 +76,8 @@ resource "aws_directory_service_log_subscription" "active_directory" {
   directory_id   = aws_directory_service_directory.active_directory.id
   log_group_name = aws_cloudwatch_log_group.active_directory.name
   depends_on = [
-    aws_cloudwatch_log_resource_policy.active_directory-log-policy
+    aws_cloudwatch_log_resource_policy.active_directory-log-policy,
+    aws_directory_service_directory.active_directory
   ]
 }
 
