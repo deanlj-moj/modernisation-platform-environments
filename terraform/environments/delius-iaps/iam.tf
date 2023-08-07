60059ee39af216ba4bbf2da1a63eb8aeb61c7d71
Seb Norris <sebastian@automationlogic.com>
2023-08-07 11:38:08 +0100

    remove iaps_ds_arn variable due to failure

--- terraform/environments/delius-iaps/iam.tf
+++ terraform/environments/delius-iaps/iam.tf
@@ -10,7 +10,6 @@ resource "aws_iam_role" "ci_secrets_rotator" {
 }
 
 locals {
-  iaps_ds_arn              = "arn:aws:ds:eu-west-2:${data.aws_caller_identity.current.account_id}:directory/${aws_directory_service_directory.active_directory.id}"
   iaps_ds_admin_secret_arn = aws_secretsmanager_secret.ad_password.arn
 }
 
