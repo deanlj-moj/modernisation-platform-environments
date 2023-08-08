7c845f78783637247624fc47710c64006817cfc5
Seb Norris <sebastian@automationlogic.com>
2023-08-08 14:24:12 +0100

    add test block for db secret name

--- terraform/environments/delius-iaps/rds.tf
+++ terraform/environments/delius-iaps/rds.tf
@@ -8,6 +8,9 @@ resource "aws_db_instance" "iaps" {
 
   username                    = local.application_data.accounts[local.environment].db_user
   manage_master_user_password = true
+  master_user_secret {
+    secret_arn = "arn:aws:secretsmanager:eu-west-2:247467087019:secret:delius-iaps-db-password"
+  }
   snapshot_identifier         = length(data.aws_ssm_parameter.iaps_snapshot_data_refresh_id.value) > 0 ? data.aws_ssm_parameter.iaps_snapshot_data_refresh_id.value : null
   db_subnet_group_name        = aws_db_subnet_group.iaps.id
   vpc_security_group_ids      = [aws_security_group.iaps_db.id]
